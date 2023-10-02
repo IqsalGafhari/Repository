097a26acfa927a723bd27b784c7aa2789a239c9b
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-10-02 12:11:14 +0700

    AccountRole.cs

--- Repository/Models/AccountRole.cs
+++ Repository/Models/AccountRole.cs
@@ -2,20 +2,15 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookingManagementApp.Models
-{
-    //membuat anotasi tabel dengan nama custom
+{//nama tabel akun role
     [Table("tb_m_account_roles")]
     public class AccountRole : BaseEntity
-    {
-        //tidak boleh null, menamai kolom dan tipe data spesifik
+    {//nama kolom dan tipe data
         [Required, Column("account_guid")]
         public Guid AccountGuid { get; set; }
-        //tidak boleh null, menamai kolom dan tipe data spesifik
         [Required, Column("role_guid")]
         public Guid RoleGuid { get; set; }
-
         public Account? Account { get; set; }
         public Role? Role { get; set; }
-
     }
 }
