7e89ac3b2182dd2ae357058d12d2c87689bfd00a
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-10-02 12:10:17 +0700

    Account.cs

--- Repository/Models/Account.cs
+++ Repository/Models/Account.cs
@@ -3,24 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookingManagementApp.Models
 {
-    //membuat anotasi tabel dengan nama custom
-    [Table("tb_m_accounts")]
+    [Table("tb_m_accounts")]//nama tabel account
     public class Account : BaseEntity
-    {
-        //tidak boleh null, menamai kolom dan tipe data spesifik
+    {//nama column dan tipe data
         [Required, Column("password", TypeName = "nvarchar")]
         public string Password { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("otp")]
         public int OTP { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("is_used")]
         public bool IsUsed { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("expired_time")]
         public DateTime ExpiredTime { get; set; }
         public Employee? Employee { get; set; }
         public ICollection<AccountRole>? AccountRoles { get; set; }
-
     }
 }
