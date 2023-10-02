4c1b5b531fce2d561a0caf49497bbaaf8660d1ba
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-10-02 12:13:24 +0700

    Booking.cs

--- Repository/Models/Booking.cs
+++ Repository/Models/Booking.cs
@@ -3,24 +3,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookingManagementApp.Models
-{
-    //membuat anotasi tabel dengan nama custom
+{//nama tabel booking
     [Table("tb_tr_bookings")]
     public class Booking : BaseEntity
-    {
-        //tidak boleh null dan menamai kolom 
+    {//nama kolom dan tipe data
         [Required, Column("start_date")]
         public DateTime StartDate { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("status")]
         public StatusLevel Status { get; set; }
-        //tidak boleh null, menamai kolom dan tipe data spesifik
         [Required, Column("remarks", TypeName = "nvarchar")]
         public string Remarks { get; set; } 
-        //tidak boleh null dan menamai kolom 
         [Required, Column("employee_guid")]
         public Guid EmployeeGuid { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("room_guid")]
         public Guid RoomGuid { get; set; }
         public Employee? Employee { get; set; }
