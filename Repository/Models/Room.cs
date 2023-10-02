834af71e7664de08eae979cfa755aab60af20c7f
J3C218193 <72546948+J3C218193@users.noreply.github.com>
2023-10-02 12:17:29 +0700

    Room.cs

--- Repository/Models/Room.cs
+++ Repository/Models/Room.cs
@@ -2,18 +2,14 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookingManagementApp.Models
-{
-    //membuat anotasi tabel dengan nama custom
+{//nama tabel room
     [Table("tb_m_rooms")]
     public class Room : BaseEntity
-    {
-        //tidak boleh null, menamai kolom dan tipe data spesifik
+    {//nama kolom dan tipe data
         [Required, Column("name", TypeName = "nvarchar(100)")]
         public string Name { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("floor")]
         public int Floor { get; set; }
-        //tidak boleh null dan menamai kolom 
         [Required, Column("capacity")]
         public int Capacity { get; set; }
         public ICollection<Booking>? Bookings { get; set; }
